d97ae55ad3deb9d1dc6c17267db05fbf7b6131ea
ananysagar <sagaranany577@gmail.com>
2025-02-02 16:53:25 +0530

    setting up searcbar and googlemap grid structure

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
-import SideNavbar from '@/components/SideNavbar';
-import 'tailwindcss/tailwind.css'
+import SideNavbar from "@/components/SideNavbar";
+import "tailwindcss/tailwind.css";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex">
       <SideNavbar />
+      <div className="grid grid-cols-1 md:grid-cols-2 px-6 md:px-10 w-full mt-10">
+        <div>Business List</div>
+        <div>Google Map</div>
+      </div>
     </div>
   );
 }
